73936457c0a78863384b1102904a5706f8857ed3
Mihir Laud <mihirlaud@hotmail.com>
2018-06-22 12:24:11 -0400

    Changed motor ports

--- TLC_750R/include/globals.h
+++ TLC_750R/include/globals.h
@@ -1,9 +1,9 @@
-#define D_MOTOR_1 1
-#define D_MOTOR_2 2
+#define D_MOTOR_1 3
+#define D_MOTOR_2 8
 #define D_MOTOR_3 9
-#define D_MOTOR_4 10
+#define D_MOTOR_4 2
 
-#define INTAKE_MOTOR 3
+#define INTAKE_MOTOR 5
 
 #define LAUNCHER_MOTOR 4
 
