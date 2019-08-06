a446d58eebf4c2ad9469020553e1c3b1cf39dd6c
RahulBhatt24 <rbhatt06@gmail.com>
2019-08-05 20:05:28 -0400

    Update config.h

--- TT_750R/include/config.h
+++ TT_750R/include/config.h
@@ -1,7 +1,9 @@
 #include "vex.h"
 #include <vector>
 
-#define D_MOTOR_FL vex::PORT14
-#define D_MOTOR_BL vex::PORT11
-#define D_MOTOR_FR vex::PORT19
-#define D_MOTOR_BR vex::PORT20
+#define D_MOTOR_FL       vex::PORT14
+#define D_MOTOR_BL       vex::PORT12
+#define D_MOTOR_FR       vex::PORT19
+#define D_MOTOR_BR       vex::PORT15
+#define RIGHT_LIFT_MOTOR vex::PORT18
+#define LEFT_LIFT_MOTOR  vex::PORT13
