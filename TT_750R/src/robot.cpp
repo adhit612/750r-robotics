2c7831aaa736be7befa4ec328b433b49312da570
RahulBhatt24 <rbhatt06@gmail.com>
2019-11-23 11:46:27 -0500

    Update robot.cpp

--- TT_750R/src/robot.cpp
+++ TT_750R/src/robot.cpp
@@ -22,9 +22,9 @@ void Robot::turnRight() {
 	for(int i = 0; i < driveMotors.size(); i++) {
 		vex::motor m = driveMotors.at(i);
     if(i%2==0) 
-			m.spin(vex::directionType::fwd, 25, vex::percentUnits::pct); //LEFT SIDE
+			m.spin(vex::directionType::fwd, 10, vex::percentUnits::pct); //LEFT SIDE
     else
-      m.spin(vex::directionType::rev, 25, vex::percentUnits::pct); //RIGHT SIDE
+      m.spin(vex::directionType::rev, 10, vex::percentUnits::pct); //RIGHT SIDE
 	}
 }
 
@@ -32,9 +32,9 @@ void Robot::turnLeft() {
 	for(int i = 0; i < driveMotors.size(); i++) {
 		vex::motor m = driveMotors.at(i);
     if(i%2==0)
-			m.spin(vex::directionType::rev, 25, vex::percentUnits::pct); //LEFT SIDE
+			m.spin(vex::directionType::rev, 10, vex::percentUnits::pct); //LEFT SIDE
     else
-      m.spin(vex::directionType::fwd, 25, vex::percentUnits::pct); //RIGHT SIDE
+      m.spin(vex::directionType::fwd, 10, vex::percentUnits::pct); //RIGHT SIDE
 	}
 }
 
@@ -61,7 +61,7 @@ void Robot::lift(int mode)
 void Robot::moveMagazine(double mode)
 {
   vex::motor m = vex::motor(MAGAZINE_MOTOR);
-  m.spin(vex::directionType::fwd,50*mode,vex::percentUnits::pct);
+  m.spin(vex::directionType::fwd,25*mode,vex::percentUnits::pct);
   if(mode==0)
   {
     m.stop(vex::brakeType::hold);
@@ -74,6 +74,12 @@ void Robot::intake(int mode)
   vex::motor m2 = vex::motor(RIGHT_ROLLER_MOTOR, true);
   m.spin(vex::directionType::fwd,100*mode,vex::percentUnits::pct);
   m2.spin(vex::directionType::fwd,100*mode,vex::percentUnits::pct);
+
+  if(mode==0)
+  {
+    m.stop(vex::brakeType::hold);
+    m2.stop(vex::brakeType::hold);
+  }
 }
 
 void Robot::driveFor(double distance, double velocity) {
@@ -91,8 +97,8 @@ void Robot::turnFor(double angle) { //POSITIVE VALUE = TURN LEFT
 
 void Robot::intakeFor(double distance, int mode)
 {
-  vex::motor m = vex::motor(LEFT_ROLLER_MOTOR);
-  vex::motor m2 = vex::motor(RIGHT_ROLLER_MOTOR, false);
+  vex::motor m = vex::motor(LEFT_ROLLER_MOTOR, false);
+  vex::motor m2 = vex::motor(RIGHT_ROLLER_MOTOR, true);
   m.rotateFor(vex::directionType::fwd, distance*mode, vex::rotationUnits::deg, false);
   m2.rotateFor(vex::directionType::fwd, distance*mode, vex::rotationUnits::deg, true);
 }
@@ -100,11 +106,31 @@ void Robot::intakeFor(double distance, int mode)
 void Robot::magazineFor(int mode)
 {
   vex::motor m = vex::motor(MAGAZINE_MOTOR);
-  m.rotateFor(vex::directionType::fwd, 50*mode, vex::rotationUnits::deg, true);
+  m.rotateFor(vex::directionType::fwd, 1000*mode, vex::rotationUnits::deg, true);
 }
 
 void Robot::intakeAndMove(double distance, double velocity,int mode)
 {
-  intakeFor(distance, mode);
-  driveFor(distance, velocity);
+  vex::motor m = vex::motor(LEFT_ROLLER_MOTOR,false);
+  vex::motor m2 = vex::motor(RIGHT_ROLLER_MOTOR, true);
+  driveMotors.at(0).rotateFor(vex::directionType::fwd, distance, vex::rotationUnits::deg, velocity, vex::velocityUnits::pct, false);
+  driveMotors.at(1).rotateFor(vex::directionType::fwd, distance, vex::rotationUnits::deg, velocity, vex::velocityUnits::pct, false);
+  m.rotateFor(vex::directionType::fwd, distance*mode, vex::rotationUnits::deg, false);
+  m2.rotateFor(vex::directionType::fwd, distance*mode, vex::rotationUnits::deg, true);
+}
+
+void Robot::liftTo(int mode)
+{
+  vex::motor m = vex::motor (RIGHT_LIFT_MOTOR,false);
+  vex::motor m2 = vex::motor (LEFT_LIFT_MOTOR, true);
+  m.rotateFor(vex::directionType::fwd, 100*mode, vex::rotationUnits::deg, 75, vex::velocityUnits::pct, false);
+  m2.rotateFor(vex::directionType::fwd, 100*mode, vex::rotationUnits::deg, 75, vex::velocityUnits::pct, false);
+  //m.startRotateTo(2238, vex::rotationUnits::deg, 175*mode, vex::velocityUnits::pct);
+  //m2.startRotateTo(-1980, vex::rotationUnits::deg, 175*mode, vex::velocityUnits::pct);
+
+  if(mode==0)
+  {
+    m.stop(vex::brakeType::hold);
+    m2.stop(vex::brakeType::hold);
+  }
 }
